fdf393c20aa22f208a8bcbe2df4104f5411bea1e
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2019-05-02 06:10:20 +0530

    :tada: 13 Commit

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 /* eslint-disable no-console */
-function doSomething(arr) {
-  arr.map((elt, i) => {
-    console.log(elt);
-    arr[i] = arr[i] * 2;
-  });
-  console.log(arr);
+// function doSomething(arr) {
+//   arr.map((elt, i) => {
+//     console.log(elt);
+//     arr[i] = arr[i] * 2;
+//   });
+//   console.log(arr);
+// }
+
+// doSomething([12, 122, 1, 13, 0]);
+
+function binarySearch(low, high, arr, key) {
+  if (low <= high) {
+    let mid = Math.floor(low + (high - low) / 2);
+    //let mid = (high - low + 1) / 2;
+    if (arr[mid] == key) {
+      console.log('found');
+      return 1;
+    }
+    if (arr[mid] > key) {
+      return binarySearch(low, mid - 1, arr, key);
+    } else {
+      return binarySearch(mid + 1, high, arr, key);
+    }
+  }
+  return -1;
 }
 
-doSomething([12, 122, 1, 13, 0]);
+console.log(binarySearch(0, 5, [1, 2, 3, 4, 5, 6], 3));
