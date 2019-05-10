40ada21e233d2cc06ca1ee3d0d313c278b479ba9
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2019-05-11 00:36:47 +0530

    :tada: Added binarySearch and insertionSort utility functions...

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
 /* eslint-disable no-console */
-
 const express = require('express');
+const bodyParser = require('body-parser');
+const { binarySearch, insertionSort } = require('./utils');
 const app = express();
-function doSomething(arr) {
-  arr.map((elt, i) => {
-    console.log(elt);
-    arr[i] = arr[i] * 2;
-  });
-  console.log(arr);
-}
 
-doSomething([12, 122, 1, 13, 0]);
+//Set up middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
-function binarySearch(low, high, arr, key) {
-  if (low <= high) {
-    let mid = Math.floor(low + (high - low) / 2);
-    //let mid = (high - low + 1) / 2;
-    if (arr[mid] == key) {
-      console.log('found');
-      return 1;
-    }
-    if (arr[mid] > key) {
-      return binarySearch(low, mid - 1, arr, key);
-    } else {
-      return binarySearch(mid + 1, high, arr, key);
-    }
-  }
-  return -1;
-}
-
-console.log(binarySearch(0, 5, [12, 22, 23, 24, 25, 66], 66));
-
+app.use(bodyParser.json());
 app.get('/', (req, res) => {
   res.json({
     data: 'yummy'
   });
 });
 
-const port = process.env.port || 4000;
+app.post('/binarySearch', (req, res) => {
+  console.log(req.body);
+  const { data, key } = req.body;
+  const status = binarySearch(0, data.length - 1, data, key);
+  if (status === 1) {
+    res.json({ status: 'found' });
+  } else {
+    res.json({ status: 'Not found' });
+  }
+});
+
+app.post('/insertionSort', (req, res) => {
+  const { data } = req.body;
+  // console.log(typeof data);
+  const result = insertionSort(data);
+  console.log(result);
+  res.json(result);
+});
+
+const port = process.env.port || 4400;
 app.listen(port, () => {
-  console.log('Server running on 4000');
+  console.log(`Server running on ${port}`);
 });
---
+++ utils.js
@@ -0,0 +1,33 @@
+const binarySearch = (low, high, arr, key) => {
+  if (low <= high) {
+    let mid = Math.floor(low + (high - low) / 2);
+    //let mid = (high - low + 1) / 2;
+    if (arr[mid] == key) {
+      return 1;
+    }
+    if (arr[mid] > key) {
+      return binarySearch(low, mid - 1, arr, key);
+    } else {
+      return binarySearch(mid + 1, high, arr, key);
+    }
+  }
+  return -1;
+};
+
+const insertionSort = unsorted => {
+  let arr = unsorted;
+  let len = arr.length - 1;
+  for (let i = 1; i < len; i++) {
+    let key = arr[i];
+    let j = i - 1;
+
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j = j - 1;
+    }
+    arr[j + 1] = key;
+  }
+  return arr;
+};
+
+module.exports = { binarySearch, insertionSort };
